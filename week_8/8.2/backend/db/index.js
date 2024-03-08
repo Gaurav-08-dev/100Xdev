7b3f37ae12434d6a01fe09f02adb895dc4dca44c
gaurav <g8111997@gmail.com>
2024-03-08 19:13:20 +0530

    create account schema for paytm backend

--- week_8/8.2/backend/db/index.js
+++ week_8/8.2/backend/db/index.js
@@ -1,16 +1,52 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
 const UserSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  password: String,
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+
+const AccountSchema = new mongoose.Schema({
+  balance: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: "true",
+  },
 });
 
 const User = mongoose.model("User", UserSchema);
-
+const Account = mongoose.model("Account", AccountSchema);
 module.exports = {
   User,
+  Account,
 };
