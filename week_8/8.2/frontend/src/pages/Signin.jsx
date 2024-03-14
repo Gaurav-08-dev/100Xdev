cb5bcaef23b46c720e8a38ff608eef254dac89ec
gaurav <g8111997@gmail.com>
2024-03-15 00:15:33 +0530

    signin implemented

--- week_8/8.2/frontend/src/pages/Signin.jsx
+++ week_8/8.2/frontend/src/pages/Signin.jsx
@@ -4,18 +4,41 @@ import BottomWarning from "../components/BottomWarning";
 import Button from "../components/Button";
 import InputBox from "../components/InputBox";
 import AuthStructure from "../components/AuthStructure";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignin = () => {
+    axios
+      .post("http://localhost:3069/api/v1/user/signin", {
+        username: email,
+        password,
+      })
+      .then((res) => {
+        localStorage.setItem("user-token", res.data.token);
+        navigate("/dashboard");
+      });
+  };
 
-  }
   return (
     <AuthStructure>
       <Heading label="Sign in" />
       <Subheading label="Enter your credentials to access your account" />
-      <InputBox label="Email" placeholder="Enter Email" />
-      <InputBox label="Password" placeholder="Enter Password" />
+      <InputBox
+        onChange={(e) => setEmail(e.target.value)}
+        label="Email"
+        placeholder="Enter Email"
+      />
+      <InputBox
+        onChange={(e) => setPassword(e.target.value)}
+        label="Password"
+        placeholder="Enter Password"
+      />
       <div className="pt-4">
         <Button label="Sign In" onClick={() => handleSignin()} />
       </div>
