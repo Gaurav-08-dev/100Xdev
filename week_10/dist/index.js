46df9cdb06a0348c095c0eac8efbca16835fd1f8
gaurav <g8111997@gmail.com>
2024-03-19 01:41:06 +0530

    postgress

---
+++ week_10/dist/index.js
@@ -0,0 +1,72 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const client = new pg_1.Client({
+    connectionString: "postgresql://Gaurav-08-dev:ws1j2vZAtHCN@ep-raspy-bonus-a5ey23id.us-east-2.aws.neon.tech/week_10?sslmode=require",
+});
+function createUsersTable() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.connect();
+        const result = yield client.query(`
+  CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  );
+`);
+        console.log(result);
+    });
+}
+// createUsersTable();
+function insertData(username, email, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            // 1. Create the query
+            const insertQUery = "INSERT INTO users (username, email,password) VALUES ($1, $2, $3)";
+            // 2. values to be inserted
+            const values = [username, email, password];
+            // 3. Make the query
+            const res = yield client.query(insertQUery, values);
+            console.log("Data Inserted Successfully", res);
+        }
+        catch (error) {
+            console.log("Error during insertion", error);
+        }
+        finally {
+            yield client.end();
+        }
+    });
+}
+// insertData("gaurav", "gaurav@gaurav.com", "gauravas").catch(console.error);
+function getUser(email) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            const query = "SELECT * FROM users WHERE email = $1";
+            const values = [email];
+            const res = yield client.query(query, values);
+            console.log("User Found", res.rows);
+            return null;
+        }
+        catch (error) {
+            console.log("Error during insertion", error);
+            throw error;
+        }
+        finally {
+            yield client.end();
+        }
+    });
+}
+// getUser("gaurav@gaurav.com")
---
+++ week_10/src/index.ts
@@ -0,0 +1,61 @@
+import { Client } from "pg";
+
+const client = new Client({
+  connectionString:
+    "postgresql://Gaurav-08-dev:ws1j2vZAtHCN@ep-raspy-bonus-a5ey23id.us-east-2.aws.neon.tech/week_10?sslmode=require",
+});
+
+async function createUsersTable() {
+  await client.connect();
+  const result = await client.query(`
+  CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  );
+`);
+  console.log(result);
+}
+
+// createUsersTable();
+async function insertData(username: string, email: string, password: string) {
+  try {
+    await client.connect();
+    // 1. Create the query
+    const insertQUery =
+      "INSERT INTO users (username, email,password) VALUES ($1, $2, $3)";
+
+    // 2. values to be inserted
+    const values = [username, email, password];
+
+    // 3. Make the query
+    const res = await client.query(insertQUery, values);
+    console.log("Data Inserted Successfully", res);
+  } catch (error) {
+    console.log("Error during insertion", error);
+  } finally {
+    await client.end();
+  }
+}
+
+// insertData("gaurav", "gaurav@gaurav.com", "gauravas").catch(console.error);
+
+async function getUser(email: string) {
+  try {
+    await client.connect();
+    const query = "SELECT * FROM users WHERE email = $1";
+    const values = [email];
+    const res = await client.query(query, values);
+    console.log("User Found", res.rows);
+    return null;
+  } catch (error) {
+    console.log("Error during insertion", error);
+    throw error
+  } finally {
+    await client.end();
+  }
+}
+
+// getUser("gaurav@gaurav.com")
