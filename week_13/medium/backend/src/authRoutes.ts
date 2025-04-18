e422869b9846c3dff7840376eeaf793a0a82ae5d
gaurav <g8111997@gmail.com>
2025-04-19 02:01:37 +0530

    Updates

--- week_13/medium/backend/src/authRoutes.ts
+++ week_13/medium/backend/src/authRoutes.ts
@@ -1,14 +1,22 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const auth = new Hono();
+const auth = new Hono<{
+    Bindings:{
+        DATABASE_URL:string // for typecheck env vars
+    }
+}>();
 
 auth.post("/user/signup", (c) => {
-return c.text("hello hono")
-})
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  return c.text("hello hono");
+});
 
 auth.post("/user/signin", (c) => {
-    return c.text("hello hono")
-})
+  return c.text("hello hono");
+});
 
 export default auth;
-
--- week_13/medium/backend/src/index.ts
+++ week_13/medium/backend/src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import auth from "./authRoutes";
 import blog from "./blogroutes";
+
+
 const app = new Hono();
 
 app.get("/", (c) => {
